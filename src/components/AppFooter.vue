f1e2b95a69634609bf1b0c25d67beafec73b58a7
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-08-29 16:08:09 +0200

    [UPD] Base structure for the footer section is done

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,11 +5,55 @@ export default {
 </script>
 
 <template>
+<footer>
+    <!-- brand logos -->
+     <div>
+        <div class="container1200">
+            <div class="content d-flex wrap">
+                <div class="col-12 h-50 layout-db"></div>
+                <div class="col-12 h-100 layout-db"></div>
+            </div>
+        </div>
+     </div>
+    <!-- subscribe! -->
     <div>
-        
+        <div class="container1200">
+            <div class="content d-flex">
+                <div class="col-12 h-100 layout-db"></div>
+            </div>
+        </div>
     </div>
+    <!-- main footer -->
+    <div>
+        <div class="container1200">
+            <div class="content d-flex">
+                <div class="col-12 h-400 layout-db"></div>
+            </div>
+        </div>
+    </div>
+    <!-- bottom footer -->
+    <div>
+        <div class="container1200">
+            <div class="content d-flex">
+                <div class="col-12 h-75 layout-db"></div>
+            </div>
+        </div>
+    </div>
+</footer>
 </template>
 
 <style lang="scss">
-    
+@use "../styles/partials/variables";
+
+.h-50{
+    height: 50px;
+}
+
+.h-100{
+    height: 150px;
+}
+
+.h-75{
+    height: 75px;
+}
 </style>
