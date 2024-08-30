feafe1e6084aea75268c7fcb1085ee429e5c5bae
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-08-30 12:30:21 +0200

    [UPD] First section of main is fully stilized

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,9 +8,16 @@ export default {
 <main>
     
     <!--new arrival's-->
-    <div>
+    <div class="brand-jumbo ">
         <div class="container1200">
-            <div class="content d-flex flex-column h-400 layout-db"></div>
+            <div class="content d-flex flex-column align-center">
+                <h1>Brand New Arrivals</h1>
+                <span>NEW COLLECTION FROM NEW YORK</span>
+                <div class="d-flex">
+                    <button class="btn-glassy mr-20">VIEW ALL</button>
+                    <button class="btn-glassy">LOOKBOOK</button>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -90,6 +97,37 @@ export default {
 .box{
     height: 300px;
     width: calc(100% / 2);
+}
+
+.brand-jumbo{
+    background-image: url(../assets/images/home1_slide_three_bg_2.jpg);
+    background-size: contain;
+    background-size: 100%;
+    background-repeat: no-repeat;
+
+    .content{
+        padding: 185px 0;
+
+        h1{
+            color: white;
+            font-size: 50pt ;
+        }
+
+        span {
+            font-size: 16pt;
+            margin: 10px 0 20px;
+        }
+
+        .btn-glassy{
+            background-color: rgba(255, 255, 255, 0.158);
+            color: white;
+            padding: 10px 25px;
+            border: 2px solid white;
+            border-radius: 20px;
+        }
+    }
+
+
 }
 
 </style>
